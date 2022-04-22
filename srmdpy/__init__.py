fb4e420788527b89a464720a5b44ee176314513f
Nicholas Richardson <njericha@uwaterloo.ca>
2022-04-22 11:10:13 -0400

    Update __init__.py

--- srmdpy/__init__.py
+++ srmdpy/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 
-from srmdpy import SRMD
-import utils
-import visualization
-import constants
+from .srmdpy import SRMD
+import .utils
+import .visualization
+import .constants
